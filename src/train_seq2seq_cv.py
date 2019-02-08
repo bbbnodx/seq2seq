6406adface124807547c67a315dfeb6f2f58240d
nozot <nozoe-tatsuya@ksk.co.jp>
2019-02-08 13:04:43 +0900

    modify train script

--- src/train_seq2seq_cv.py
+++ src/train_seq2seq_cv.py
@@ -14,10 +14,11 @@ from attention_seq2seq import AttentionSeq2seq
 
 # ファイルパスの設定
 dataset_dir =Path('./dataset')
-# source_csv = dataset_dir / "addition.csv"
 source_csv = dataset_dir / "interpretation_train43_and_test40.csv"
 result_dir = Path('../result')
 dataset_name = source_csv.stem
+model_dir = Path('../model')
+encoding = 'utf-8'
 
 # TextSequenceクラスからCSV読み込み
 seq = TextSequence()
@@ -32,21 +33,20 @@ batch_size = 32
 max_epoch = 50
 max_grad = 5.0
 
-is_reverse = True  # Reverse input
-
+# shuffle
 x, t = seq.shuffle(seed=1)
 
-for k, (x_train, x_test, t_train, t_test) in enumerate(seq.cv_dataset_gen(x, t, test_size=0.2)):
-    if is_reverse:
-        x_train, x_test = x_train[:, ::-1], x_test[:, ::-1]
+# K-分割交差法(test_size = 1/K)
+K = 4
 
+for i, (x_train, x_test, t_train, t_test) in enumerate(seq.cv_dataset_gen(x, t, test_size=1/K)):
     # モデル選択(パラメータの初期化)
     # model = Seq2seq(vocab_size, wordvec_size, hidden_size)
     model = PeekySeq2seq(vocab_size, wordvec_size, hidden_size)
     # model = AttentionSeq2seq(vocab_size, wordvec_size, hidden_size)
     optimizer = Adam()
     trainer = Trainer(model, optimizer)
-    print("Cross Validation: iter", str(k+1))
+    print("Cross Validation: iter", str(i+1))
     # Train
     trainer.fit(x_train, t_train, x_test, t_test,
                 max_epoch=max_epoch,
@@ -58,16 +58,12 @@ for k, (x_train, x_test, t_train, t_test) in enumerate(seq.cv_dataset_gen(x, t, 
     sample_size = seq.t_length
     guess_train = model.generate(x_train, start_id, sample_size)
     guess_test = model.generate(x_test, start_id, sample_size)
-    # guess_train, sum_cf_train = model.generate(x_train, start_id, sample_size)
-    # guess_test, sum_cf_test = model.generate(x_test, start_id, sample_size)
 
     # Save result as csv
-    encoding = 'utf-8'
-    suffix = "_cv" + str(k + 1)
+    suffix = "_cv" + str(i + 1)
     result_train_csv = result_dir / ("result_" + dataset_name + "_" + model.__class__.__name__ + "_train"+ suffix +".csv")
     result_test_csv = result_dir / ("result_" + dataset_name + "_" + model.__class__.__name__ + "_test"+ suffix +".csv")
-    if is_reverse:
-        x_train, x_test = x_train[:, ::-1], x_test[:, ::-1]  # 逆順を元の順に戻す
+
     seq.result_to_csv(result_train_csv, x_train, t_train, guess_train, encoding=encoding)
     seq.result_to_csv(result_test_csv, x_test, t_test, guess_test, encoding=encoding)
 
